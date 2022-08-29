07c83a31a34a717ad2e6a15984e590ca9c3b69bc
FPS\bhaurao.more <bhaurao.more@nihilent.com>
2022-08-29 11:01:43 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
-
+import  {} from 'bootstrap'; 
+import {BrowserRouter as Routres} from 'react-router-dom'
 function App() {
   return (
+    <Routres>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>E-Com Project</h1>
     </div>
+    </Routres>
   );
 }
 
